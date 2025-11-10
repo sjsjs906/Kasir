09816b3cfdfb93a746be9b603ef393ecf175980e
ALI <alialbuni337@gmail.com>
2025-11-10 12:23:29 +0700

    Add files via upload

---
+++ Kasir2.php
@@ -0,0 +1,13 @@
+<?php  
+echo "Data Pemberian Barang";  
+?>  
+
+<br>  
+<br>  
+Nama Pemberi : <?php echo $pemberi; ?>  
+<br>  
+Nama Barang : <?php echo $barang; ?>  
+<br>  
+Jumlah : <?php echo $jumlah; ?>  
+<br>  
+Harga : <?php echo $harga; ?>
